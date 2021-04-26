931bcbfe21a25f0b12f2b5a035748311d01e6f3c
EndRollModel <51425629+EndRollModel@users.noreply.github.com>
2021-04-26 11:47:34 +0800

    first upload

---
+++ app/src/main/java/com/sample/app/android_basics_kotlin/unit1/a/CreateBirthDayCard.kt
@@ -0,0 +1,13 @@
+package com.sample.app.android_basics_kotlin.unit1.a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sample.app.android_basics_kotlin.R
+
+class CreateBirthDayCard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_birth_day_card)
+
+    }
+}
